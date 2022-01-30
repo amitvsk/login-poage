9180f8ba6387c6b6873ce457c8706b02a8636908
amitvsk <83784108+amitvsk@users.noreply.github.com>
2022-01-30 20:47:20 +0530

    Add files via upload

--- registation/login.js
+++ registation/login.js
@@ -5,12 +5,24 @@ var Name=document.getElementById("Name").value;
 var Email=document.getElementById("email").value;
 var Password=document.getElementById("pd").value;
 var Confirmpassword=document.getElementById("npd").value;
+if(Confirmpassword == Password){
+    alert("saved password");
+}
+else{
+    alert("unmatch password");
+}
+console.log('about to fetch a formdata');
+        fetch('http://saranshapi.herokuapp.com/auth/?user=&pass=')
+            .then(Response => {
+               alert(Response['status']);
+            })
+
 alert("Name");
 }
 function index2(){    
 var Username=document.getElementById("uname").value;
 var lpassword=document.getElementById("psw").value;
 alert("Username");
- }
+}
 
 
